4e75dcc560ec0a3d17ba3be4a4bcafe495a7b366
FUFU222 <tnkakr_0817@icloud.com>
2024-07-23 22:48:29 +0900

    tab fin sp 10%

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
-// scripts.js
 document.querySelectorAll('.faq__list__item__question').forEach(question => {
   question.addEventListener('click', () => {
     const answer = question.nextElementSibling;
     const isOpen = answer.classList.contains('open');
-    
+    const arrow = question.querySelector('.faq__list__item__question__arrow svg');
+
     // すべてのアコーディオン項目を閉じる
     document.querySelectorAll('.faq__list__item__answer').forEach(ans => {
       ans.classList.remove('open');
     });
 
-
     document.querySelectorAll('.faq__list__item__question').forEach(q => {
       q.classList.remove('open');
+      const arrowIcon = q.querySelector('.faq__list__item__question__arrow svg');
+      if (arrowIcon) {
+        arrowIcon.classList.remove('rotate');
+      }
     });
 
     // クリックした項目を開く
     if (!isOpen) {
       answer.classList.add('open');
       question.classList.add('open');
+      if (arrow) {
+        arrow.classList.add('rotate');
+      }
     }
   });
 });
